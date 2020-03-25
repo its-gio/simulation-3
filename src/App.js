3672a52e82634d908f141c7f2d984334f11fa65f
Giovani Aguirre <me@its-g.io>
2020-03-25 14:41:53 -0700

    Created component skeletons, routers file, and redux folder with empty reducer and store.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.scss';
+import Nav from './Components/Nav';
+import Auth from './Components/Auth';
+import Dashboard from './Components/Dashboard';
+import Form from './Components/Form';
+import Post from './Components/Post';
 
 function App() {
   return (
     <div>
-      Warking
+      <Nav />
+      <Auth />
+      <Dashboard />
+      <Form />
+      <Post />
     </div>
   );
 }
