b848f79589317e5229c230590b6c73cc83ae62b1
Giovani Aguirre <me@its-g.io>
2020-03-26 14:38:07 -0700

    Form setup for Auth.jsx page.

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 export default class Auth extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div>
-        Auth
+        <form className="auth-form">
+          <legend>
+            Username:
+            <input onChange={this.handleInput} name="username" value={this.state.username} />
+          </legend>
+
+          <legend>
+            Password:
+            <input onChange={this.handleInput} name="password" value={this.state.password} type="password" />
+          </legend>
+
+          <button>Login</button>
+          <button>Register</button>
+        </form>
       </div>
     )
   }
