f76e8e09dc29077afa6d7d9f4ab7ccfb104122cd
Giovani Aguirre <me@its-g.io>
2020-03-26 13:01:28 -0700

    Updated seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS simulation_users;
 DROP TABLE IF EXISTS simulation_posts;
+DROP TABLE IF EXISTS simulation_users;
 
 CREATE TABLE simulation_users
 (
   user_id SERIAL PRIMARY KEY,
-  usesrname VARCHAR(20) UNIQUE NOT NULL,
+  username VARCHAR(20) UNIQUE NOT NULL,
   password TEXT UNIQUE NOT NULL,
   profile_pic TEXT
 );
