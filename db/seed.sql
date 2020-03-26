537d85f4ab87974e43f4d15048a8cb621dfffcc6
Giovani Aguirre <me@its-g.io>
2020-03-26 09:34:42 -0700

    Updated seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -5,10 +5,15 @@ CREATE TABLE simulation_users
 (
   user_id SERIAL PRIMARY KEY,
   usesrname VARCHAR(20) UNIQUE NOT NULL,
-  password TEXT UNIQUE NOT NULL
+  password TEXT UNIQUE NOT NULL,
+  profile_pic TEXT
 );
 
 CREATE TABLE simulation_posts
 (
-  post_id SERIAL PRIMARY KEY
+  post_id SERIAL PRIMARY KEY,
+  author_id INTEGER REFERENCES simulation_users(user_id),
+  title VARCHAR(45),
+  img TEXT,
+  content TEXT
 );
