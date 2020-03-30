4e948ba6a97ba593d58f1bb36e2d882abe4c56c8
Giovani Aguirre <me@its-g.io>
2020-03-30 16:06:00 -0700

    Created query for title funtionality server-side. Postman Approved.

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -16,15 +16,20 @@ async function createPost(req, res) {
 
 async function getPosts(req, res) {
   const db = req.app.get('db');
+  const { title } = req.query;
   let gottenPosts;
 
   try{
     gottenPosts = await db.posts.get_posts();
+    
+    if (title) {
+      const foundPosts = gottenPosts.filter(post => post.title.toLowerCase().includes(title.toLowerCase()));
+      return foundPosts.length !== 0 ? res.status(200).json(foundPosts) : res.status(200).json(gottenPosts);
+    }
   } catch (err) {
     return res.status(500).json(err);
   }
-
-  res.status(200).json(gottenPosts);
+  return res.status(200).json(gottenPosts);
 }
 
 module.exports = {
